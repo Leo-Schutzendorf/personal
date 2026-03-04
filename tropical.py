cba779796416d41fd4b220165fd5a42926e20ba3
Leo-Schutzendorf <168584606+Leo-Schutzendorf@users.noreply.github.com>
2026-03-04 15:59:22 -0700

    ?

---
+++ tropical.py
@@ -0,0 +1,12 @@
+import numpy as np
+def main():
+    expression = input("Type expression: ").strip()
+    term_list = []
+    for var in range (len(expression)-1):
+        term = ""
+        term += expression[var]
+
+    
+
+if __name__== "__main__":
+    main()
