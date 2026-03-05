9d0bdc60af1100460ef3a5c378c34eeea1e60f6a
Leo-Schutzendorf <168584606+Leo-Schutzendorf@users.noreply.github.com>
2026-03-04 23:47:02 -0700

    Add files via upload

--- tropical.py
+++ tropical.py
@@ -1,12 +1,24 @@
+import re
 import numpy as np
+def insert_char_slicing(string, char, position):
+    # s[:n] gets characters from start to index n (exclusive)
+    # s[n:] gets characters from index n (inclusive) to the end
+    return string[:position] + char + string[position:]
+
 def main():
     expression = input("Type expression: ").strip()
-    term_list = []
-    for var in range (len(expression)-1):
-        term = ""
-        term += expression[var]
+    for char in expression:
+        if char == "-":
+            expression = insert_char_slicing(expression, "+", expression.index(char))
+    firstGetMin = re.split(r"\+", expression)
 
-    
+    print(firstGetMin)
+
+    getMin=[]
+    for term in firstGetMin:
+        getMin.append(term.split("*"))
+
+    print(getMin)
 
 if __name__== "__main__":
     main()
